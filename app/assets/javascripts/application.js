8f4312bdf73e119607ec18fd5c941922061f76f6
paoloferraro99 <paoloferraro99@gmail.com>
2014-12-15 22:03:23 +0000

    for miki

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,25 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function Blocmetrics ()
+{
+
+}
+
+Blocmetrics.track = function(name, property_1, property_2){
+  var _bm_event = {
+      name: "site name",
+      property_1: "some value",
+      property_2: "some value"
+      }
+
+  var _bm_request = new XMLHttpRequest();
+  _bm_request.open("POST", "http://localhost:3000/events.json", true);
+  _bm_request.setRequestHeader('Content-Type', 'application/json');
+  _bm_request.onreadystatechange = function() {
+    // this function runs when the Ajax request changes state.
+    // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+  };
+  _bm_request.send(JSON.stringify(_bm_event));
+};
